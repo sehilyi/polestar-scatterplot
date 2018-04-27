bb1c6bcbfcba66d33f63ed25c3ab1d88cf12f1d9
Shaheen Sharifian <shaheen.sharifian@gmail.com>
2018-04-27 00:25:58 -0700

    Fix popup closing bug & add some mark channel

--- src/components/encoding-pane/property-editor-schema.ts
+++ src/components/encoding-pane/property-editor-schema.ts
@@ -16,13 +16,22 @@ export interface StringSchema {
   type: 'string';
   title: string;
   enum?: string[];
+  default?: string;
+}
+
+export interface IntegerSchema {
+  type: 'integer';
+  title: string;
+  minimum: number;
+  maximum: number;
+  multipleOf: number;
 }
 
 export function isStringSchema(schema: SchemaProperty): schema is StringSchema {
   return schema.type === 'string';
 }
 
-export type SchemaProperty = ObjectSchema | StringSchema;
+export type SchemaProperty = ObjectSchema | StringSchema | IntegerSchema;
 
 export interface UISchema {
   [key: string]: UISchemaItem;
@@ -53,6 +62,14 @@ const DEFAULT_SELECT_UISCHEMA: UISchemaItem = {
   'ui:emptyValue': 'auto'
 };
 
+const DEFAULT_COLOR_UISCHEMA: UISchemaItem = {
+  'ui:widget': 'color'
+};
+
+const DEFAULT_INTEGER_RANGE_UISCHEMA: UISchemaItem = {
+  'ui:widget': 'range'
+};
+
 const POSITION_FIELD_QUANTITATIVE_INDEX = {
   'Common': [
     {
@@ -78,7 +95,14 @@ const POSITION_FIELD_NOMINAL_INDEX = {
 
 const POSITION_FIELD_TEMPORAL_INDEX = {
   'Scale': ['type'].map(p => ({prop: 'scale', nestedProp: p})),
-  'Axis': ['orient', 'title'].map(p => ({prop: 'axis', nestedProp: p}))
+  'Axis': ['orient', 'title'].map(p => ({prop: 'axis', nestedProp: p})) };
+
+const COLOR_INDEX = {
+  'Color': ['fillOpacity', 'opacity'].map(p => ({prop: 'color', nestedProp: p}))
+};
+
+const SIZE_INDEX = {
+  'Size': ['size'].map(p => ({prop: p}))
 };
 
 // Capitalize first letter for aesthetic purposes in form
@@ -132,6 +156,23 @@ export function getPropertyEditorSchema(prop: string, nestedProp: string, propTa
       type: 'string'
     };
     return generateSchema(prop, nestedProp, DEFAULT_SELECT_UISCHEMA, propTab, schemaProperty);
+  } else if (prop === 'size') {
+    const schemaProperty: StringSchema = {
+      title: title,
+      type: 'string'
+    };
+    return generateSchema(prop, nestedProp, DEFAULT_TEXT_UISCHEMA, propTab, schemaProperty);
+  } else if (prop === 'color') {
+    if (nestedProp === 'fillOpacity' || nestedProp === 'opacity') {
+      const schemaProperty: IntegerSchema = {
+        title: title,
+        type: 'integer',
+        minimum: 0,
+        maximum: 1,
+        multipleOf: 0.1
+      };
+      return generateSchema(prop, nestedProp, DEFAULT_INTEGER_RANGE_UISCHEMA, propTab, schemaProperty);
+    }
   } else {
     throw new Error('Property combination not recognized');
   }
@@ -159,7 +200,7 @@ function generateSchema(prop: string, nestedProp: string, uiSchemaItem: UISchema
 }
 
 export function getFieldPropertyGroupIndex(shelfId: ShelfId, fieldDef: ShelfFieldDef) {
-  if (shelfId.channel === Channel.X || shelfId.channel === Channel.Y) {
+  if (fieldDef && (shelfId.channel === Channel.X || shelfId.channel === Channel.Y)) {
     switch (fieldDef.type) {
       case ExpandedType.QUANTITATIVE:
         return POSITION_FIELD_QUANTITATIVE_INDEX;
@@ -170,6 +211,10 @@ export function getFieldPropertyGroupIndex(shelfId: ShelfId, fieldDef: ShelfFiel
       case ExpandedType.TEMPORAL:
         return POSITION_FIELD_TEMPORAL_INDEX;
     }
+  } else if (shelfId.channel === Channel.COLOR) {
+    return COLOR_INDEX;
+  } else if (shelfId.channel === Channel.SIZE) {
+    return SIZE_INDEX;
   }
 }
 
@@ -181,7 +226,12 @@ export function generateFormData(shelfId: ShelfId, fieldDef: ShelfFieldDef) {
       const prop = customProp.prop;
       const nestedProp = customProp.nestedProp;
       const propertyKey = nestedProp ? prop + '_' + nestedProp : prop;
-      formData[propertyKey] = fieldDef[prop] ? nestedProp ? fieldDef[prop][nestedProp] : fieldDef[prop] : undefined;
+      if (shelfId.channel === Channel.X || shelfId.channel === Channel.Y) {
+        formData[propertyKey] = fieldDef[prop] ? nestedProp ? fieldDef[prop][nestedProp] : fieldDef[prop] : undefined;
+      } else {
+        // mark
+        // TODO: Make explicit if case for mark encodings
+      }
     }
   }
 
