1e45d5cc87cf0be5dd46d11644b8e9a2ca7f78c1
sehilyi <shlyi@hcil.snu.ac.kr>
2018-10-10 16:32:55 +0900

    Clean codes

--- src/components/guide-pane/guide-element.tsx
+++ src/components/guide-pane/guide-element.tsx
@@ -2,21 +2,14 @@ import * as React from 'react';
 import * as CSSModules from 'react-css-modules';
 
 import * as styles from "./guide-element.scss";
-import * as warn from '../../../images/warning.png';
-import * as tip from '../../../images/tip.png';
-import * as done from '../../../images/done.png';
-import * as ignore from '../../../images/ignore.png';
-import {GuidelineItemTypes, GuidelineItemActionableCategories, isAllowedScatterplot, getGuidedSpec, GuidelineItem, Guidelines} from '../../models/guidelines';
+import {GuidelineItemTypes, GuidelineItemActionableCategories, getGuidedSpec} from '../../models/guidelines';
 import {ActionHandler} from '../../actions';
-import {GuidelineAction, GUIDELINE_SHOW_RECT_INDICATOR, GUIDELINE_HIDE_INDICATOR, GUIDELINE_TOGGLE_IGNORE_ITEM, GUIDELINE_TOGGLE_ISEXPANDED} from '../../actions/guidelines';
-import {Schema, ShelfUnitSpec, ShelfFilter, filterIndexOf, filterHasField} from '../../models';
+import {GuidelineAction} from '../../actions/guidelines';
+import {Schema, ShelfUnitSpec, ShelfFilter} from '../../models';
 import {InlineData} from 'vega-lite/build/src/data';
 import {FacetedCompositeUnitSpec} from 'vega-lite/build/src/spec';
 import {Themes} from '../../models/theme/theme';
-import {OneOfFilter} from '../../../node_modules/vega-lite/build/src/filter';
-import {COLOR, SHAPE} from '../../../node_modules/vega-lite/build/src/channel';
 import {ActionableOverplotting} from './actionable-pane/actionable-overplotting';
-import {showContourInD3Chart, hideContourInD3Chart} from '../../models/d3-chart';
 import {StudySetting} from '../../models/study';
 
 export interface GuideElementProps extends ActionHandler<GuidelineAction> {
@@ -56,7 +49,6 @@ export class GuideElementBase extends React.PureComponent<GuideElementProps, Gui
     if (this.state.isExpanded === true && this.props.item.guideState == "IGNORE")
       this.setState({isExpanded: false});
 
-    const {guideState, content, title, subtitle} = this.props.item;
 
     return (
       <div styleName={this.state.isExpanded ? "expanded" : "guideline"}>
@@ -68,7 +60,7 @@ export class GuideElementBase extends React.PureComponent<GuideElementProps, Gui
   }
   private renderActionablePane() {
     const {id} = this.props.item;
-    const {item, schema, spec, handleAction, data, mainSpec, theme, filters, guidelines, studySetting} = this.props;
+    const {item, schema, handleAction, data, mainSpec, theme, filters, guidelines, studySetting} = this.props;
 
     switch (id) {
       case 'GUIDELINE_OVER_PLOTTING': {
